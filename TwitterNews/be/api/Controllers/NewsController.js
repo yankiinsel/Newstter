f050bac9aab400c743ee518aecc362643f40887c
Yanki Insel <yankiinsel@gmail.com>
2020-08-08 02:42:15 +0300

    newsApi made private library

--- TwitterNews/be/api/Controllers/NewsController.js
+++ TwitterNews/be/api/Controllers/NewsController.js
@@ -1,7 +1,7 @@
 const config = require('../Config.js');
 
 exports.getNews = (req, res) => {
-    const NewsAPI = require('newsapi');
+    const NewsAPI = require.main.require('./libraries/newsapi');
     const newsapi = new NewsAPI(config.config.newsClient.key);
     newsapi.v2.everything({
         q: req.query.topic,
