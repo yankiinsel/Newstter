9a48afb29ea97f143a4b38c91c0f4b34d792ba87
Yanki Insel <yankiinsel@gmail.com>
2020-08-08 00:19:13 +0300

    serverless implemented

--- TwitterNews/be/api/routes/index.js
+++ TwitterNews/be/api/routes/index.js
@@ -1,17 +1,18 @@
-module.exports = (app) => {
+module.exports = (app, router) => {
     // Bind the controller to the route
     const mapController = require('../Controllers/MapController.js');
     const topicsController = require('../Controllers/TopicsController.js');
     const newsController = require('../Controllers/NewsController.js');
 
+    router.get('/', (req, res) => { 
+        res.json({
+            'hello': 'hi'
+        })
+    })
 
-    app.route('/map')
-        .get(mapController.getAvailableTrends)
+    router.get('/map', mapController.getAvailableTrends)
+    router.get('/topics', topicsController.getTopics)
+    router.get('/news', newsController.getNews)
 
-    app.route('/topics/')
-        .get(topicsController.getTopics)
-
-    app.route('/news/')
-        .get(newsController.getNews)
-        
+    app.use('/.netlify/functions/app', router);
 };
--- TwitterNews/be/app.js
+++ TwitterNews/be/app.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 3001;
 const routes = require('./api/routes/index.js'); // Importing route definitions
 
 const app = express();
+const router = express.Router();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Binding our routes with the server instance
-routes(app);
+routes(app, router);
 
 app.listen(PORT, () => console.log('App running @' + PORT));
+module.exports.handler = serverless(app);
